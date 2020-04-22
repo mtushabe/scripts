ff7068af566a24cfef17d70b6f3743a5728dd934
Moses Tushabe <41683375+mtushabe@users.noreply.github.com>
2020-04-22 18:16:06 +0300

    Update teitransfer.py

--- teitransfer.py
+++ teitransfer.py
@@ -1,23 +1,30 @@
 import pandas as pd
 import json
 from dhis2 import Api
-api = Api("http://prep.mets.or.ug","admin","district")
-data = pd.read_csv('lugore.csv')
+#Don't forget to put the right credentials to the system and also look at the TRACKER program you working with.
+api = Api("http://localhost:8181","admin","district")
+#Change the path to the file with the ids
+data = pd.read_csv('mul.csv')
 uids= data['id'].astype(str).tolist()
-for i in uids:
-    result=api.put('tracker/ownership/transfer',params={'trackedEntityInstance':i,'program':'F5ZdOBuqlJh','ou':'oSbVEcIxwTX'})
-    print (i +" "+ result.json()["message"])
-print("Step 1 Done!")
-
-for item in uids:
-    teiResult = api.get('trackedEntityInstances',params={'trackedEntityInstance':item,'fields':'*'})
-    trackedEntities = teiResult.json()['trackedEntityInstances']
-    dict_entity=dict()
-    for entity in trackedEntities:
+transferFrom = "Facility A"
+transferTo = "Facility B"
+fromOrg = api.get('organisationUnits',params={'field':'id','filter':'name:eq:'+ transferFrom})
+toOrg = api.get('organisationUnits',params={'field':'id','filter':'name:eq:'+ transferTo})
+if not fromOrg.json()['organisationUnits']:
+    print("Transfer from organisation unit not found!")
+elif not toOrg.json()['organisationUnits']:
+    print("Transfer from organisation unit not found!")
+else:
+    transferFromId = fromOrg.json()['organisationUnits'][0]['id']
+    transferToId = toOrg.json()['organisationUnits'][0]['id']
+    for item in uids:
+        result = api.put('tracker/ownership/transfer',params={'trackedEntityInstance':item,'program':'F5ZdOBuqlJh','ou':transferToId})
+        teiResult = api.get('trackedEntityInstances',params={'trackedEntityInstance':item,'fields':'*'})
+        entity = teiResult.json()['trackedEntityInstances'][0]
         tei_json =json.dumps(entity)
-        tei_json = tei_json.replace("Am5BLrPGyfR","oSbVEcIxwTX")
-        tei_json = tei_json.replace("Facility A","Lugore Prisons HC II (Patiko)")
+        tei_json = tei_json.replace(transferFromId,transferToId)
+        tei_json = tei_json.replace(transferFrom,transferTo)
         dict_entity = json.loads(tei_json)
-    transfer=api.put('trackedEntityInstances/'+item,data = dict_entity)
-    print (item +" "+ transfer.json()["message"])
-print("Step 2 Done!")
+        transfer = api.put('trackedEntityInstances/'+item,data = dict_entity)
+        print (item +" "+ result.json()["message"])
+    print("Transfer process done!")
