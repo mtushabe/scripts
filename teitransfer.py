e7848aac81852dac822cedbb22514241fa995d77
Moses Tushabe <41683375+mtushabe@users.noreply.github.com>
2020-04-22 15:47:14 +0300

    Create teitransfer.py

---
+++ teitransfer.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import json
+from dhis2 import Api
+api = Api("http://prep.mets.or.ug","mtushabe","Justborn@08")
+data = pd.read_csv('lugore.csv')
+uids= data['id'].astype(str).tolist()
+for i in uids:
+    result=api.put('tracker/ownership/transfer',params={'trackedEntityInstance':i,'program':'F5ZdOBuqlJh','ou':'oSbVEcIxwTX'})
+    print (i +" "+ result.json()["message"])
+print("Step 1 Done!")
+
+for item in uids:
+    teiResult = api.get('trackedEntityInstances',params={'trackedEntityInstance':item,'fields':'*'})
+    trackedEntities = teiResult.json()['trackedEntityInstances']
+    dict_entity=dict()
+    for entity in trackedEntities:
+        tei_json =json.dumps(entity)
+        tei_json = tei_json.replace("Am5BLrPGyfR","oSbVEcIxwTX")
+        tei_json = tei_json.replace("Facility A","Lugore Prisons HC II (Patiko)")
+        dict_entity = json.loads(tei_json)
+    transfer=api.put('trackedEntityInstances/'+item,data = dict_entity)
+    print (item +" "+ transfer.json()["message"])
+print("Step 2 Done!")
