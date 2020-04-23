e4ef623af674f3762c870a2ccbca747b0c4040b7
Moses Tushabe <41683375+mtushabe@users.noreply.github.com>
2020-04-23 18:46:09 +0300

    Create pruner.py

---
+++ pruner.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+from dhis2 import Api
+api = Api("http://prep.mets.or.ug","admin","district")
+data = pd.read_csv('deletion.csv')
+clientIds= data['unique'].astype(str).tolist()
+
+for item in clientIds:
+    teiResult=api.get('trackedEntityInstances',params={'ou':'akV6429SUqu','ouMode':'DESCENDANTS','filter':'wk44sbe36Wm:eq:'+item,'fields':'*'})
+    trackedEntityInstances = teiResult.json()['trackedEntityInstances']
+    if not trackedEntityInstances:
+        print(item +" "+"is currently available!")
+    else:
+        enrollments = teiResult.json()['trackedEntityInstances'][0]['enrollments']
+        teiId = teiResult.json()['trackedEntityInstances'][0]['trackedEntityInstance']
+        if trackedEntityInstances and not enrollments:
+            response = api.delete('trackedEntityInstances/'+ teiId)
+            print(item +" "+"hanging record with this id has been removed!")
+        else:
+            print(item +" "+"has enrollments attached to it!")
+
+print("Cleaning process done!")
