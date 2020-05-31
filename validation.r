9102ea98185569f9850ea7a6670ccd5739a7495d
Moses Tushabe <41683375+mtushabe@users.noreply.github.com>
2020-05-31 20:36:32 +0300

    Create validation.r

---
+++ validation.r
@@ -0,0 +1,12 @@
+library(datimvalidation)
+require(datimvalidation)
+secrets<-"secrets.json"
+loadSecrets(secrets)
+datafile<-"AGYW_PREV-Final.csv"
+d<-d2Parser(filename = datafile ,type = "csv")# this throws an error for AGYW_PREV since the attribute option combo used is default(HllvX50cXC0)
+#This covers all datasets including the one that has AGYW_PREV that normally throws the invalid data elements error
+checkDataElementOrgunitValidity(data = d,datasets = c("jKdHXpBfWop","em1U5x9hhXh","qzVASYuaIey","BPEyzcDb8fT","mbdbMiLZ4AA"))
+checkDataElementDisaggValidity(data=d,datasets = c("jKdHXpBfWop","em1U5x9hhXh","qzVASYuaIey","BPEyzcDb8fT","mbdbMiLZ4AA"))
+checkValueTypeCompliance(d)
+checkNegativeValues(d)
+Violations_AGYW_PREV <-validateData(data = d, datasets = c("jKdHXpBfWop","em1U5x9hhXh","qzVASYuaIey","mbdbMiLZ4AA","BPEyzcDb8fT"),return_violations_only = TRUE)
