3d0600929bcacf36331e7fdf545cf33fbbeae662
Moses Tushabe <41683375+mtushabe@users.noreply.github.com>
2021-06-06 09:04:05 +0300

    Create kptransposer.py

---
+++ kptransposer.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+datae = pd.read_csv("refill.csv")
+ids=datae[["Unique Client Number"]].copy()
+ids.drop_duplicates(keep="first",inplace=True)
+template = []
+#for i in ids.index:
+for idx, identifier in ids.iterrows():
+    filtered = datae[datae["Unique Client Number"]==identifier["Unique Client Number"]]
+    temp={}
+    temp["id"]=identifier["Unique Client Number"]
+    event=0
+    for index, row in filtered.iterrows():
+        temp["event."+str(event)]=row["Event date"]
+        event=event+1    
+    template.append(temp)
+final=pd.DataFrame(template)
+final.to_csv("df_refill.csv",index=False)   
